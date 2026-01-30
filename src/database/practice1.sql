c444281128c3426b494835e084f137e5fb3d82b0
junhyunsim0402 <simjjang11@gmail.com>
2026-01-30 11:25:00 +0900

    practice문제 저장
    저장하지않고 커밋&푸시 해버림

--- src/database/practice1.sql
+++ src/database/practice1.sql
@@ -1,3 +1,13 @@
 -- 문제1
 create database my_db;
-
+-- 문제2
+show databases;
+-- 문제3
+use my_db;
+-- 문제4
+drop database my_db;
+-- 문제5
+drop database if exists company_db;
+create database company_db;
+use company_db;
+show databases;
